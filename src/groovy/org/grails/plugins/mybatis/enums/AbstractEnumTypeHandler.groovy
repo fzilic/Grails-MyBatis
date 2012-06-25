50278a9386b752dcfe7dd85b68cc5695ea7d3cbf
Franjo Žilić <frenky666@gmail.com>
2012-06-25 13:06:02 +0200

    #1 Unimplemented method in AETH

--- src/groovy/org/grails/plugins/mybatis/enums/AbstractEnumTypeHandler.groovy
+++ src/groovy/org/grails/plugins/mybatis/enums/AbstractEnumTypeHandler.groovy
@@ -1,13 +1,13 @@
 package org.grails.plugins.mybatis.enums
 
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import java.sql.CallableStatement
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.util.List
 
-import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.BaseTypeHandler
+import org.apache.ibatis.type.JdbcType
 
 abstract class AbstractEnumTypeHandler<T extends Enum> extends BaseTypeHandler<T> {
 
@@ -20,27 +20,30 @@ abstract class AbstractEnumTypeHandler<T extends Enum> extends BaseTypeHandler<T
   }
 
   private enumFromProperty(value) {
-    def foundEnum = enumClass.find {
-      it[enumProperty] == value
-    }
+    def foundEnum = enumClass.find { it[enumProperty] == value }
     return foundEnum
+  }
+
+  @Override
+  public T getNullableResult(CallableStatement statement, int columnIndex) throws SQLException {
+    def source = statement.getObject(columnIndex)
+    return enumFromProperty(source)
+  }
+
+  @Override
+  public T getNullableResult(ResultSet resultSet, int columnIndex) throws SQLException {
+    def source = resultSet.getObject(columnIndex)
+    return enumFromProperty(source)
+  }
+
+  @Override
+  public T getNullableResult(ResultSet resultSet, String columnName) throws SQLException {
+    def source = resultSet.getObject(columnName)
+    return enumFromProperty(source)
   }
 
   @Override
   public void setNonNullParameter(PreparedStatement statement, int index, T parameter, JdbcType jdbcType) throws SQLException {
     statement.setObject(index, parameter[enumProperty])
   }
-
-  @Override
-  public T getNullableResult(ResultSet resultSet, String columnName) throws SQLException {
-    def source = resultSet.getObject(columnName)
-    return enumFromProperty(source);
-  }
-
-  @Override
-  public T getNullableResult(CallableStatement statement, int columnIndex) throws SQLException {
-    def source = statement.getObject(columnIndex)
-    return enumFromProperty(source);
-  }
-
 }
